e2a8e07fb38b5e0e7de9cf837c4289427485e9e8
joe <jsph.niu@gmail.com>
2015-08-04 16:56:32 -0600

    ServiceProvider

---
+++ src/NachoServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace hakula\nacho;
+
+use Illuminate\Support\ServiceProvider;
+
+class NachoServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
